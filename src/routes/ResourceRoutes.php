21b924be0d709cf60be8a49ed8d54d377f816030
schery19 <44877783+schery19@users.noreply.github.com>
2024-07-01 23:43:52 -0400

    Add files via upload
    Ajout de resourcesRoutes

---
+++ src/routes/ResourceRoutes.php
@@ -0,0 +1,51 @@
+<?php
+
+// Tous droits réservés GRAVITY-CORE 2024
+// Distribués sous licence MIT
+// Voir le fichier LICENSE.txt pour plus de détails
+
+namespace Gravity\Core\Routing;
+
+
+abstract class ResourceRoutes {
+
+    /** 
+	 * Les routes en GET supportées par la ressource
+	 * @var array<Route>
+	 */
+    public $get = [];
+
+	/** 
+	 * Les routes en POST supportées par la ressource
+	 * @var array<Route>
+	 */
+    public $post = [];
+
+	/** 
+	 * Les routes en PUT supportées par la ressource
+	 * @var array<Route>
+	 */
+    public $put = [];
+
+	/** 
+	 * Les routes en PATCH supportées par la ressource
+	 * @var array<Route>
+	 */
+    public $patch = [];
+
+	
+	protected Router $router;
+
+
+	abstract function __construct();
+
+
+	public function __set($name, $value) {
+		if($name == 'router')
+			$this->router = $value;
+	}
+
+}
+
+
+?>
