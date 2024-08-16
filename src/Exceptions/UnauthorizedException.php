2070300b35db7117a65cde1d23f340ac3db03109
schery19 <44877783+schery19@users.noreply.github.com>
2024-08-15 22:35:42 -0400

    Add files via upload

---
+++ src/Exceptions/UnauthorizedException.php
@@ -0,0 +1,13 @@
+<?php
+
+// Tous droits réservés GRAVITY-CORE 2024
+// Distribués sous licence MIT
+// Voir le fichier LICENSE.txt pour plus de détails
+
+namespace Gravity\Core\Exceptions;
+
+class UnauthorizedException extends \Exception {
+
+}
+
+?>
