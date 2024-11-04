ffa15b3a7a7ad23c34c5c4888f523226183d54cd
schery19 <44877783+schery19@users.noreply.github.com>
2024-11-04 15:51:07 -0500

    Update Controller.php

--- src/app/Controllers/Controller.php
+++ src/app/Controllers/Controller.php
@@ -129,13 +129,13 @@ class Controller {
 	 * Exécuter dynamiquement une fonction avec ses éventuels arguments
 	 * 
 	 * @param string $methodName le nom de la méthode du controleur exécuter
-	 * @param Router $router le router qui exécute le controleur
 	 * @param array $arguments les éventuels arguments de la méthode
+	 * @param Router $router le router qui exécute le controleur
 	 * @throws \Gravity\Core\Exceptions\ControllerException
 	 * 
 	 * @return mixed
 	 */
-	public function invoke(string $methodName, Router $router, $arguments = array()) {
+	public function invoke(string $methodName, $arguments = array(), Router $router) {
 
 		$this->router = $router;
 
